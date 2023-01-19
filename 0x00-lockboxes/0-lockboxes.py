464df4fb15f51dfc0477ac9bd6726e3cfa7e2c2e
AdamNB-sys <a1adnat@gmail.com>
2023-01-19 11:15:31 -0600

    first pld solution, some error in testing

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def canUnlockAll(boxes):
+    visited = set()
+    visited.add(0)
+    stack = [0]
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key not in visited:  # or if visited
+                visited.add(key)
+                stack.append(key)
+    return len(visited) == len(boxes)
--- 0x00-lockboxes/main_0.py
+++ 0x00-lockboxes/main_0.py
@@ -2,11 +2,36 @@
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
+# Pass
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))
 
+# Pass
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes))
 
+# Pass
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
+
+# Pass
+boxes = [[1], [2], [3], [4], [], [5], [7]]
+print(canUnlockAll(boxes))
+
+# Pass
+boxes = [[1, 4, 5, 6], [], [], [], [3, 2], [], [], [], []]
+print(canUnlockAll(boxes))
+
+# Pass
+boxes = [[1, 4, 1], [1, 4, 3], [1, 4, 6], [1, 4, 4], [1, 4, 4]]
+print(canUnlockAll(boxes))
+
+# Pass
+boxes = [[1, 4, 6], [1, 2, 6], [1, 3, 6], [1, 4, 6], [1, 4, 6], [], []]
+print(canUnlockAll(boxes))
+
+# Error out index out of range
+# integers within boxes are greater than the amount of boxes
+# if two more boxes are present, no error occurs
+boxes = [[1, 4, 6], [1, 4, 6], [1, 4, 8], [1, 4, 6], [1, 4, 6]]
 print(canUnlockAll(boxes))
