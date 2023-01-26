03c462c1c8963ee72856904dcd64ea961d923004
AdamNB-sys <a1adnat@gmail.com>
2023-01-25 22:24:27 -0600

    done and dusted

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,12 +12,5 @@ def canUnlockAll(boxes):
             # and it has not already been found
             if key not in availableKeys and key < len(boxes):
                 availableKeys.append(key)
-    # If any numbers are missing from available keys
-    # and not all boxes are open, return false
+    # Return bool result of whether keys match boxes
     return len(availableKeys) == len(boxes)
-    '''x = 0
-    while x < len(boxes):
-        if x not in availableKeys:
-            return False
-        x += 1
-    return True'''
